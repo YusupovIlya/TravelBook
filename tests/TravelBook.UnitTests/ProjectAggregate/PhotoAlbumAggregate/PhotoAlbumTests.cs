2c48b0a202693e6b10189d3610f9ae2a92dc30c2
Ilya <90928878+YusupovIlya@users.noreply.github.com>
2022-08-25 22:24:31 +0300

    UnitTests photoalbum domain model

---
+++ tests/TravelBook.UnitTests/ProjectAggregate/PhotoAlbumAggregate/PhotoAlbumTests.cs
@@ -0,0 +1,93 @@
+﻿using TravelBook.Core.ProjectAggregate;
+using Xunit;
+
+namespace TravelBook.UnitTests;
+
+public class PhotoAlbumTests
+{
+    private string _testName = "With family";
+    private int _testTravelId = 1;
+    private Photo[]? _testPhotos;
+    private PhotoAlbum? _testPhotoalbum;
+
+    private PhotoAlbum CreateAlbum()
+    {
+        return new PhotoAlbum(_testName, _testTravelId);
+    }
+
+    private Photo[] GeneratePhotosArray()
+    {
+        return new Photo[]{ 
+            new Photo("/img1.jpg"),
+            new Photo("/img2.jpg", "photo1", 1),
+            new Photo("/img3.jpg")
+        };
+    }
+
+    [Fact]
+    public void InitializesTitle()
+    {
+        // act
+        _testPhotoalbum = CreateAlbum();
+
+        // assert
+        Assert.Equal(_testName, _testPhotoalbum.Title);
+    }
+
+    [Fact]
+    public void InitializesTravel()
+    {
+        // act
+        _testPhotoalbum = CreateAlbum();
+
+        // assert
+        Assert.Equal(_testTravelId, _testPhotoalbum.TravelId);
+    }
+
+    [Fact]
+    public void AddPhotos_NotEmptyCollection_ReturnsTrue()
+    {
+        // arrange
+        _testPhotoalbum = CreateAlbum();
+        _testPhotos = GeneratePhotosArray();
+
+        // act
+        _testPhotoalbum.AddPhotos(_testPhotos);
+
+        // assert
+        Assert.NotEmpty(_testPhotoalbum.Photos);
+    }
+
+    [Fact]
+    public void AddPhotos_ContainsGivenPhoto_ReturnsTrue()
+    {
+        // arrange
+        _testPhotoalbum = CreateAlbum();
+        _testPhotos = GeneratePhotosArray();
+        Photo testPhoto = new Photo("/img3.jpg");
+
+        // act
+        _testPhotoalbum.AddPhotos(_testPhotos);
+
+        // assert
+        Assert.Contains(testPhoto, _testPhotoalbum.Photos);
+    }
+
+    [Fact]
+    public void RemovePhotos_IsEmptyCollection_ReturnsTrue()
+    {
+        // arrange
+        _testPhotoalbum = CreateAlbum();
+        _testPhotos = GeneratePhotosArray();
+
+        // act
+        _testPhotoalbum.AddPhotos(_testPhotos);
+        _testPhotoalbum.RemovePhotos(
+            new Photo("/img1.jpg"),
+            new Photo("/img2.jpg", "photo1", 1),
+            new Photo("/img3.jpg"));
+
+        // assert
+        Assert.Equal(new Photo[0], _testPhotoalbum.Photos);
+    }
+}
